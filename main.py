1e3aceb982556e202d19a198c1d026cdfa1b4559
alisabir99 <alisabir99@hotmail.com>
2025-01-05 09:22:34 +0000

    add clock

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+
     while True:
         screen.fill((0,0,0))
 
@@ -19,7 +23,7 @@ def main():
                 return
 
 
-
+        dt = clock.tick(60)/1000
         pygame.display.flip()
 
 
